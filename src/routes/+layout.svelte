f53bda6a58f8cc8c09ee44e8da18c5e30d449314
Runa <spookupt@gmail.com>
2026-04-10 00:59:05 -0700

    embed test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,6 +11,9 @@
 	<meta property="og:title" content="Runa's Portfolio :3" />
 	<meta property="og:description" content="View my portfolio here!" />
 	<meta property="og:image" content="https://runyra.pages.dev/assets/misc/preview.png" />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
+	<meta property="og:image:type" content="image/png" />
 	<meta property="og:url" content="https://runyra.pages.dev" />
 	<meta property="og:type" content="website" />
 
