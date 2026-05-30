4fbeacc30cdfb13c1534f827e71bf5b4241509ce
Runa <spookupt@gmail.com>
2026-05-30 15:46:50 -0700

    Fix preview being cached

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
 
 	<meta property="og:title" content="Runa's Portfolio :3" />
 	<meta property="og:description" content="View my portfolio & more here!" />
-	<meta property="og:image" content="https://runyra.pages.dev/assets/misc/preview.png" />
+	<meta property="og:image" content="https://runyra.pages.dev/assets/misc/preview2.png" />
 	<meta property="og:url" content="https://runyra.pages.dev" />
 	<meta property="og:type" content="website" />
 	<meta property="og:site_name" content="Runa's Portfolio"/>
@@ -18,7 +18,7 @@
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:title" content="Runa's Portfolio :3" />
 	<meta name="twitter:description" content="View my portfolio & more here!" />
-	<meta name="twitter:image" content="https://runyra.pages.dev/assets/misc/preview.png" />
+	<meta name="twitter:image" content="https://runyra.pages.dev/assets/misc/preview2.png" />
 
 
 	<meta name="theme-color" content="#880833" />
